84cce4f8ae9eed41b499e0ccf3793d42466ebf2f
dcha <dowoncha.cs@gmail.com>
2020-03-25 20:23:04 -0400

    initial commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import libtcodpy as libtcod
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+def main():
+  # libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+  libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python dnd', False)
+
+  while not libtcod.console_is_window_closed():
+    libtcod.console_set_default_foreground(0, libtcod.white)
+
+    libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+
+    libtcod.console_flush()
+
+
+if __name__ == "__main__":
+  main()
